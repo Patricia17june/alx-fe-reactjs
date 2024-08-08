d7526c1796af5848b8cc0e792031adf9bf639a19
Patricia chepkirui <pchepkirui17@gmail.com>
2024-08-08 23:18:22 +0300

    added components in src

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,4 +1,4 @@
-import WelcomeMessage from './src/WelcomeMessage'
+import WelcomeMessage from './components/WelcomeMessage'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
