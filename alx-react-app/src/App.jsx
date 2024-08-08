59479d988e686ff989244579113df867bc15ffe7
Patricia chepkirui <pchepkirui17@gmail.com>
2024-08-09 02:32:28 +0300

    implemented Header in app.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,4 +1,4 @@
-import Header from './components/Header';
+import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import WelcomeMessage from './components/WelcomeMessage'
