3438f7e447fb1696fdfda640c5df0e7cbfaf8ff2
Patricia chepkirui <pchepkirui17@gmail.com>
2024-08-09 11:42:19 +0300

    still fixing errors

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,4 +1,4 @@
-import Header from './Header'
+import Header from '.src/components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import WelcomeMessage from './components/WelcomeMessage'
