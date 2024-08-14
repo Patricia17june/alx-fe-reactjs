09c9f43f85a10593b16a4501ba6dbc2ebc394414
Patricia chepkirui <pchepkirui17@gmail.com>
2024-08-15 01:37:04 +0300

    file update

--- alx-react-app-new/src/assets/components/MainContent.jsx
+++ alx-react-app-new/src/assets/components/MainContent.jsx
@@ -1,6 +1,6 @@
 function MainContent() {
     return (
-        <main style={{backgroundColor: '#f4f4f4', padding: '15px', textAlign: 'center'}}>
+        <main style={{ backgroundColor: '#f4f4f4', padding: '15px', textAlign: 'center'}}>
             <p>I love to visit New York, Paris, and Tokyo.</p>
         </main>
     );
