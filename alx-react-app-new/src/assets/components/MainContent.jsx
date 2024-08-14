8346c96d755ba1f50f4891ba914a9678f84d5a4f
Patricia chepkirui <pchepkirui17@gmail.com>
2024-08-15 01:22:05 +0300

    added new files to alx-react-app-new

---
+++ alx-react-app-new/src/assets/components/MainContent.jsx
@@ -0,0 +1,8 @@
+function MainContent() {
+    return (
+        <main style={{backgroundColor: '#f4f4f4', padding: '15px', textAlign: 'center'}}>
+            <p>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+export default MainContent;
