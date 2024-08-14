aeeea19ee3e5f57ff21a0cce908c79c27dbf742f
Patricia chepkirui <pchepkirui17@gmail.com>
2024-08-15 02:02:38 +0300

    modified app.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,4 +1,4 @@
-import UserProfile from './components/UserProfile'
+import UserProfile from './components/Userprofile'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
