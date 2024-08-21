ee44e04b56ba5d64e5e45f9696f1904e605da01b
Patricia chepkirui <pchepkirui17@gmail.com>
2024-08-22 01:27:56 +0300

    moved recipeStore.js to components

--- recipe-sharing-app/src/components/AddRecipeForm.jsx
+++ recipe-sharing-app/src/components/AddRecipeForm.jsx
@@ -1,6 +1,6 @@
 // src/components/AddRecipeForm.js
 import React, { useState } from 'react';
-import useRecipeStore from './store/recipeStore';
+import useRecipeStore from './recipeStore';
 
 const AddRecipeForm = () => {
   const addRecipe = useRecipeStore((state) => state.addRecipe);
