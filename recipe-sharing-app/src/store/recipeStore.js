030b2454eec7b2dd0b1189f48f121f8fe68b6eeb
Patricia chepkirui <pchepkirui17@gmail.com>
2024-08-22 20:03:29 +0300

    added recipeStore.js

--- recipe-sharing-app/src/store/recipeStore.js
+++ recipe-sharing-app/src/store/recipeStore.js
@@ -1,30 +1,30 @@
 // src/store/recipeStore.js
 import create from 'zustand';
 
+// Zustand store to manage recipes
 const useRecipeStore = create((set) => ({
-  recipes: [],  // Initial state: an empty array of recipes
-
-  //action to add new recipe to the store
-  addRecipe: (newRecipe) => 
+  recipes: [], // Initial state: an empty array of recipes
+  
+  // Action to add a new recipe to the store
+  addRecipe: (newRecipe) =>
     set((state) => ({
-      //spread the existing recipes and add the new one
-    recipes: [...state.recipes, newRecipe] 
-  })),
+      recipes: [...state.recipes, newRecipe],
+    })),
+  
   // Action to update an existing recipe in the store
   updateRecipe: (updatedRecipe) =>
     set((state) => ({
-      // Use map to iterate over the recipes and replace the matching recipe
       recipes: state.recipes.map((recipe) =>
         recipe.id === updatedRecipe.id ? updatedRecipe : recipe
       ),
     })),
-    // Action to delete a recipe from the store
-  deleteRecipe: (recipeId) =>
+  
+  // Action to delete a recipe from the store
+  deleteRecipe: (id) =>
     set((state) => ({
-      // Filter out the recipe that matches the provided ID
-      recipes: state.recipes.filter((recipe) => recipe.id !== recipeId),
+      recipes: state.recipes.filter((recipe) => recipe.id !== id),
     })),
-    
 }));
 
-export default useRecipeStore;
+// Export the store to be used in other components
+export { useRecipeStore };
