a285f40b7893d5a7af50a4d1efa3c076d2c86812
Patricia chepkirui <pchepkirui17@gmail.com>
2024-08-31 16:44:56 +0300

    update Blogpost.jsx

--- react-router-advanced/src/components/Blogpost.jsx
+++ react-router-advanced/src/components/Blogpost.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 
 const BlogPost = () => {
-  const { postId } = useParams();
+  const { id } = useParams();
 
   return (
     <div>
@@ -10,6 +10,6 @@ const BlogPost = () => {
       <p>welcome to my blogpost with ID: {id}</p>
     </div>
   );
-}
+};
 
 export default BlogPost;
