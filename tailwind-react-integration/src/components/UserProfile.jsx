ec56a96f851760dfb8674cc74c1f2db1f1bf1a92
Patricia chepkirui <pchepkirui17@gmail.com>
2024-09-06 15:51:54 +0300

    adjust h and w of image respectively on sm and md screens

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -3,7 +3,7 @@ function UserProfile() {
       <div className="user-profile bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs sm:md:max-w-sm mx-auto, my-20 rounded-lg shadow-lg">
         <img src="https://via.placeholder.com/150" 
         alt="User" 
-        className="w-24 h-24 sm:w-36 h-36md: rounded-full mx-auto" />
+        className="sm:w-24 sm:h-24 md:w-36 md:h-36 rounded-full mx-auto" />
         <h1 className="text-lg sm: md:text-xl text-blue-800 my-4">John Doe</h1>
         <p className="text-sm sm:text-base text-gray-600">Developer at Example Co. Loves to write code and explore new technologies.</p>
       </div>
