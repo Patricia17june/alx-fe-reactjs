adf7960f0a70ca6d5b22b8e1bdd40b2ee22390a3
Patricia chepkirui <pchepkirui17@gmail.com>
2024-09-11 06:15:32 +0300

    modify App.jsx and add more styling to RecipeDetail.jsx

--- recipe-sharing-platform/src/components/RecipeDetail.jsx
+++ recipe-sharing-platform/src/components/RecipeDetail.jsx
@@ -9,26 +9,27 @@ const RecipeDetail = () => {
     useEffect(() => {
         const selectedRecipe = RecipesDetails.find((item)  => item.id === parseInt(id));
         setRecipe(selectedRecipe);
+        console.log(selectedRecipe);
     }, [id]);
 
     if(!recipe) {
-        return<div>Recipe not found!</div>
+        return<div className="text-center text-red-500">Recipe not found!</div>;
     }
 
     return(
-        <div className="container max-auto p-4">
+        <div className="container mx-auto p-4">
             
-            <div className="bg-white rounded-lg shadow-lg overflow hidden mt-6 p-6">
+            <div className="bg-gray-100 max-w-sm md:max-w-xl mx-auto rounded-lg shadow-lg mt-6 p-6">
                 <img 
                 src={recipe.image}
                 alt={recipe.title}
                 className="w-full h-64 object-cover rounded-lg mb-4" 
                 />
-                <h1 className="text-3xl font-bold mb-4">{recipe.title}</h1>
+                <h1 className="text-amber-600 underline text-3xl font-bold mb-4">{recipe.title}</h1>
                 <p className="text-gray-700 mb-6">{recipe.summary}</p>
                 
-                
-                <div className="mb-4">
+            
+                <div className="mb-4 font-style: italic;">
                   <h2 className="text-xl font-semibold mb-2">Ingredients</h2>
                   <ul className="list-disc list-inside">
                   {recipe.ingredients?.map((ingredient, index) => (
@@ -37,8 +38,8 @@ const RecipeDetail = () => {
                   </ul>
                 </div>
 
-
-                <div>
+                
+                <div className="mb-4">
                     <h2 className="text-xl font-semibold mb-2">Instructions</h2>
                     <ol className="list-decimal list-inside">
                         {recipe.instructions?.map((instruction, index) => (
@@ -47,7 +48,9 @@ const RecipeDetail = () => {
                     </ol>
                 </div>
             </div>
+            <div className="mt-4">
             <Link to="/" className="text-blue-500 hover:underline">Back to recipes</Link>
+            </div>
         </div>
     )
 }
